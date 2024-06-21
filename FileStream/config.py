4cd9d03d2d8a6ccfce2d2a3db4a781f37c6fe232
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-06-21 09:19:23 +0530

    latest

--- FileStream/config.py
+++ FileStream/config.py
@@ -1,25 +1,5 @@
-import logging
-from requests import get as rget
 from os import environ as env
 from dotenv import load_dotenv
-
-logging.basicConfig(level=logging.ERROR)
-LOGGER = logging.getLogger(__name__)
-
-CONFIG_FILE_URL = os.env.get('CONFIG_FILE_URL')
-try:
-    if not CONFIG_FILE_URL:
-        raise ValueError("CONFIG_FILE_URL is missing or empty")
-
-    res = rget(CONFIG_FILE_URL)
-    if res.status_code == 200:
-        with open('config.env', 'wb+') as f:
-            f.write(res.content)
-    else:
-        LOGGER.error(f"Failed to download config.env {res.status_code}")
-except Exception as e:
-    LOGGER.error(f"Error downloading CONFIG_FILE_URL: {e}")
-
 
 load_dotenv('config.env', override=True)
 
